dcdcd5c327fd219a2db5884d389eea2207526911
Mashal Hussain <2144169@talnet.nl>
2023-05-11 11:07:58 +0200

    Opdrachten van week 1

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$var1 = 12;
+$var2 = 10;
+$resultaat = $var1 + $var2;
+echo $resultaat;
+?>
+
+<?php
+$var1 = 12;
+$var2 = 10;
+$resultaat = $var1 * $var2;
+echo $resultaat;
+?>
+
+<?php
+$naam = "JouwNaam";
+echo "Welkom " . $naam . "!";
+?>
+
+<?php
+// deze code bevat veel fouten
+// haal alle fouten eruit!
+$mijnNaam = "Carl";
+$jouwNaam = "Camel";
+echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!";
+?>
